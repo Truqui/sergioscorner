709baf2f2d1d2147dc9f875152b60ae33f2e4abe
Truqui <gonzalezcruz.sergio@gmail.com>
2023-11-19 00:18:32 +0100

    Refactor Page and page content based on ckeditor

--- page/models.py
+++ page/models.py
@@ -1,19 +1,20 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.core.exceptions import ValidationError
 
 from utils.seo import SEOModel
 
 
-class HtmlPage (SEOModel):
+class Page (SEOModel):
     """A page that only render a html field.
-    Also, it has an unique name.
+    Also, it has a unique name.
     """
     name = models.CharField(
         'Name',
         max_length=50,
         unique=True
     )
-    html = models.TextField(
+    content = RichTextField(
         blank=True,
         help_text='HTML Code'
     )
--- page/test/test_views.py
+++ page/test/test_views.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.urls.base import reverse
 
-from .factories import HtmlPageFactory
+from .factories import PageFactory
 
 """
 class HtmlPageViewTest(TestCase):
 
-    def test_home_html_page(self):
+    def test_home_page(self):
         HtmlPageFactory(name='home', html='<h1>test</h1>')
         response = self.client.get(reverse('page:home'),)
 
